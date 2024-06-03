74cecbd814b89f8e826148db3f5281c88a11320e
Gustavo Batista <batistag395@gmail.com>
2024-06-03 19:15:49 -0300

    pequena refatoracao em PessoaRepository

--- src/main/java/org/example/testspringproject/repository/PessoaRepository.java
+++ src/main/java/org/example/testspringproject/repository/PessoaRepository.java
@@ -3,5 +3,4 @@ package org.example.testspringproject.repository;
 import org.example.testspringproject.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {}
